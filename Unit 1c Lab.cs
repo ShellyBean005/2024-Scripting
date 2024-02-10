d0c9a68947b73cf4102ab2c8bbb70a26c40dd3a5
ShellyBean005 <143045243+ShellyBean005@users.noreply.github.com>
2024-02-09 19:43:33 -0700

    Unit 1c lab

---
+++ Unit 1c Lab.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_1C_lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a number, and i will generate a Pyramid");
+
+            int x = Convert.ToInt32(Console.ReadLine()); //user input converted to int
+
+            for (int i = 0; i <= x; i++) //repeat every number up to the input number and making it 'int i'
+            {
+            
+                for (int y = 1; y <= i ; y++) //using 'int i' to loop it 'i' amount of times
+                   
+                    Console.Write(i + " "); //write 'i' with space, 'i' amount of times
+
+                Console.WriteLine("\n"); //to next line
+            }
+            Console.ReadLine(); //read user imput to keep window open
+        }
+    }
+}
