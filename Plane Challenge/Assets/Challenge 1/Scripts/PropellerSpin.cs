1ea7237a8de2318e1fcfe4af243d0163455c1191
Shelby <shelbel005@gmail.com>
2024-02-28 20:23:46 -0700

    Plane Challenge

---
+++ Plane Challenge/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    private float propellerSpeed = 2000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,propellerSpeed * Time.deltaTime);
+    }
+}
