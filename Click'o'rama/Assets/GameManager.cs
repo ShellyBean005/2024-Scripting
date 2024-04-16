6efd6bc94e0d7b29f5f0fff826f4c7397684bc9e
Shelby <shelbel005@gmail.com>
2024-04-15 20:31:25 -0600

    Click O rama

---
+++ Click'o'rama/Assets/GameManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private float spawnRate = 1.0f;
+    public List<GameObject> targets;
+    public int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    public GameObject titleScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        titleScreen.SetActive(false);
+        spawnRate /= difficulty;
+    }
+    public void GameOver()
+    {
+     
+         gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        Debug.Log("Game Over");
+        restartButton.gameObject.SetActive(true);
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
